6a5ca0e4f842b799283cc9424c280977c6c3d35d
Aswathy N <aswathia.achu@gmail.com>
2024-01-31 16:31:45 +0530

    Appstore banner component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 import Services from './components/Services/Services'
 import Banner from './components/Banner/Banner'
+import AppStore from './components/AppStore/AppStore'
 
 const App = () => {
   useEffect(() => {
@@ -24,6 +25,7 @@ const App = () => {
         <Home />
         <Services />
         <Banner />
+        <AppStore />
       </div>
  
     </>
